306bc748c388a8a76d12f0f4cd1876db4beb2f84
Ivan Kockarevic <kockarevic.ivan@gmail.com>
2019-09-26 13:15:08 +0200

    Added default import for utils module.

--- demo/src/main.js
+++ demo/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 // Setting custom messages.
-import messages from './vue-val/messages'
+import messages from 'vue-val/messages'
 
 messages.required = () => 'Nein.'
 messages.maxLength = (maxCharacters) => `Maximum of the maximum is ${maxCharacters} characters.`,
--- utils.js
+++ utils.js
@@ -21,3 +21,8 @@ export const validate = (formObject) => {
     
     return true;
 };
+
+export default {
+    validate,
+    set
+};
