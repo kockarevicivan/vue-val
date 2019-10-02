ba6c2e310dc6256319212559d5d78a7a526ad922
Ivan Kockarevic <kockarevic.ivan@gmail.com>
2019-10-02 11:36:15 +0200

    Polish and documentation.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,62 @@
 import messages from "./messages"
 import lambdas from "./lambdas"
 
+// Facade for everything that will be exported by the module.
 const facade = {
+    /** 
+     * Checks whether value is Falsy, or, if value is a string, whether it's Falsy when trimmed.
+     */
     required: (value) => ({ valid: lambdas.required(value), message: messages.required() }),
+
+    /** 
+     * Checks whether value is explicitly null.
+     */
     notNull: (value) => ({ valid: lambdas.notNull(value), message: messages.notNull() }),
-    maxValue: (max) => (value) => ({ valid: lambdas.maxValue(max)(value), message: messages.maxValue(max) }),
-    minValue: (min) => (value) => ({ valid: lambdas.minValue(min)(value), message: messages.minValue(min) }),
-    maxLength: (max) => (value) => ({ valid: lambdas.maxLength(max)(value), message: messages.maxLength(max) }),
-    minLength: (min) => (value) => ({ valid: lambdas.minLength(min)(value), message: messages.minLength(min) }),
+
+    /** 
+     * Checks whether value is greater than the provided maximum.
+     */
+    maxValue: (maximum) => (value) => ({ valid: lambdas.maxValue(maximum)(value), message: messages.maxValue(maximum) }),
+
+    /** 
+     * Checks whether value is lower than the provided minimum.
+     */
+    minValue: (minimum) => (value) => ({ valid: lambdas.minValue(minimum)(value), message: messages.minValue(minimum) }),
+
+    /**
+     * Checks whether value is shorter in length than provided maximum.
+     */
+    maxLength: (maximumCharacters) => (value) => ({ valid: lambdas.maxLength(maximumCharacters)(value), message: messages.maxLength(maximumCharacters) }),
+
+    /**
+     * Checks whether value is longer in length than provided minimum.
+     */
+    minLength: (minimumCharacters) => (value) => ({ valid: lambdas.minLength(minimumCharacters)(value), message: messages.minLength(minimumCharacters) }),
+
+    /**
+     * Checks whether value is a member of the provided values list.
+     */
     isValue: (values) => (value) => ({ valid: lambdas.isValue(values)(value), message: messages.isValue(values) }),
+
+    /**
+     * Checks whether value is a valid e-mail address.
+     */
     isEmail: (value) => ({ valid: lambdas.isEmail(value), message: messages.isEmail() }),
+
+    /**
+     * Checks whether value is explicitly numeric.
+     */
     isNumeric: (value) => ({ valid: lambdas.isNumeric(value), message: messages.isNumeric() }),
+
+    /**
+     * Checks whether value is lower than the provided minimum.
+     */
     isUrl: (value) => ({ valid: lambdas.isUrl(value), message: messages.isUrl() }),
 
+
+    /**
+     * Utility function for form field setting and auto-validation.
+     */
     set: (key, value, formObject) => {
         formObject[key].value = value;
         formObject[key].error = null;
@@ -29,6 +73,10 @@ const facade = {
             }
         }
     },
+
+    /**
+     * Utility function for validation of entire form.
+     */
     validate: (formObject) => {
         for(let field in formObject)
             if(!formObject[field].valid) return false;    
@@ -37,6 +85,7 @@ const facade = {
     }
 }
 
+// Validation methods export.
 export const required = facade.required;
 export const notNull = facade.notNull;
 export const maxValue = facade.maxValue;
@@ -48,9 +97,11 @@ export const isEmail = facade.isEmail;
 export const isNumeric = facade.isNumeric;
 export const isUrl = facade.isUrl;
 
+// Utility methods export.
 export const set = facade.set;
 export const validate = facade.validate;
 
+// Default export to supress warnings.
 export default {
     required: facade.required,
     notNull: facade.notNull,
@@ -64,11 +115,9 @@ export default {
     isUrl: facade.isUrl,
 
     set: facade.set,
-    validate: facade.validate,
-
-    // messages: messages,
-    // lambdas: lambdas
+    validate: facade.validate
 };
 
+// Export for imported packages (used for custom lambdas and messages).
 export { default as messages } from './messages';
 export { default as lambdas } from './lambdas';
--- src/lambdas.js
+++ src/lambdas.js
@@ -1,10 +1,11 @@
+// List of default validation lambas, dependant on 1 (always value) or more parameters (e.g. maxCharacters).
 export default {
     required: (value) => !!value && (typeof value == 'string' ? !!(value.length && value.trim().length) : true),
     notNull: (value) => value != null,
-    maxValue: (max) => (value) => value <= max,
-    minValue: (min) => (value) => value >= min,
-    maxLength: (max) => (value) => value ? value.length <= max : true,
-    minLength: (min) => (value) => value ? value.length >= min : true,
+    maxValue: (maximum) => (value) => value <= maximum,
+    minValue: (minimum) => (value) => value >= minimum,
+    maxLength: (maximumCharacters) => (value) => value ? value.length <= maximumCharacters : true,
+    minLength: (minimumCharacters) => (value) => value ? value.length >= minimumCharacters : true,
     isValue: (values) => (value) => values.indexOf(value) != -1,
     isEmail: (value) => value && /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value),
     isNumeric: (value) => value && !isNaN(value),
